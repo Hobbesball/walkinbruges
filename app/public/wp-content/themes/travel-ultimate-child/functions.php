bfcd93db8080b075cbf465bc0322a0b390f4b4e0
Calvinwuyts <jolanwuyts@gmail.com>
2019-09-12 16:06:23 +0200

    edited front page, added logos

--- app/public/wp-content/themes/travel-ultimate-child/functions.php
+++ app/public/wp-content/themes/travel-ultimate-child/functions.php
@@ -1,3 +1,13 @@
+<!-- TOC
+
+- load stylesheets
+- change section order on front page
+- enable header image on front page
+- import google fonts
+- customizing CTA front page section
+
+-->
+
 <?php
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
@@ -11,8 +21,8 @@ function enqueue_parent_styles() {
 function travel_ultimate_sortable_sections() {
     $sections = array(
         'slider'    => esc_html__( 'Slider', 'travel-ultimate' ),
-        'package'   => esc_html__( 'Package', 'travel-ultimate' ),
         'cta'    => esc_html__( 'CTA', 'travel-ultimate' ),
+        'package'   => esc_html__( 'Package', 'travel-ultimate' ),
         'search'    => esc_html__( 'Search', 'travel-ultimate' ),
         'about'     => esc_html__( 'About Us', 'travel-ultimate' ),
         'destination'     => esc_html__( 'Destination', 'travel-ultimate' ),
@@ -21,25 +31,6 @@ function travel_ultimate_sortable_sections() {
     );
     return apply_filters( 'travel_ultimate_sortable_sections', $sections );
 }
-
-// Custom 'Tour' post type
-function create_posttype() {
- 
-    register_post_type( 'tours',
-    // CPT Options
-        array(  
-            'labels' => array(
-                'name' => __( 'Tours' ),
-                'singular_name' => __( 'Tour' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'movies'),
-        )
-    );
-}
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
 
 // enabling the header image on the frontpage 
 
@@ -63,8 +54,8 @@ function travel_ultimate_header_image() {
                 <div class="wrapper">
                     <header class="page-header">
                     <div>
-					<h1 id="frontpagetitle" ><?php echo $site_name; ?></h1>
-					<p id="frontpagetagline"><?php echo esc_html( $site_description ); /* WPCS: xss ok. */ ?></p>
+                    <h1 id="frontpagetitle" ><?php echo $site_name; ?></h1>
+					<!--<p id="frontpagetagline"><?php echo esc_html( $site_description ); /* WPCS: xss ok. */ ?></p> -->
 					</div>
                         <?php 
                         //this needs some cleaning up
@@ -90,85 +81,86 @@ function travel_ultimate_header_image() {
 }
 
 
-// customizing packages section to show a custom content type
+//enable google fonts
+
+function add_google_fonts() {
+    wp_enqueue_style( 'google_web_fonts', 'https://fonts.googleapis.com/css?family=Kaushan+Script|Raleway' );
+  }
+  add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
 
 
-function travel_ultimate_get_package_section_details( $input ) {
+// customizing the CTA front page section
+
+function travel_ultimate_get_cta_section_details( $input ) {
     $options = travel_ultimate_get_theme_options();
-
-    // Content type.
-    $package_content_type  = $options['package_content_type'];
-    
     $content = array();
-    switch ( $package_content_type ) {
+    $page_id = ! empty( $options['cta_content_page'] ) ? $options['cta_content_page'] : '';
+    $args = array(
+        'post_type'         => 'page',
+        'page_id'           => $page_id,
+        'posts_per_page'    => 1,
+    );
         
-        case 'page':
-            $page_ids = array();
+    // Run The Loop.
+    $query = new WP_Query( $args );
+    if ( $query->have_posts() ) : 
+        while ( $query->have_posts() ) : $query->the_post();
+            $page_post['id']        = get_the_id();
+            $page_post['title']     = get_the_title();
+            $page_post['url']       = get_the_permalink();
+            $page_post['excerpt']   = travel_ultimate_trim_content( 88 );
+            $page_post['image']  	= has_post_thumbnail() ? get_the_post_thumbnail_url( get_the_id(), 'large' ) : '';
 
-            for ( $i = 1; $i <= 3; $i++ ) {
-                if ( ! empty( $options['package_content_page_' . $i] ) )
-                    $page_ids[] = $options['package_content_page_' . $i];
-            }
-            
-            $args = array(
-                'post_type'         => 'page',
-                'post__in'          => ( array ) $page_ids,
-                'posts_per_page'    => 3,
-                'orderby'           => 'post__in',
-                );                    
-        break;
-
-        case 'trip':
-
-            if ( ! class_exists( 'WP_Travel' ) )
-                return;
-            
-            $trip_ids = array();
-
-            for ( $i = 1; $i <= 3; $i++ ) {
-                if ( ! empty( $options['package_content_trip_' . $i] ) )
-                    $trip_ids[] = $options['package_content_trip_' . $i];
-            }
-            
-            $args = array(
-                'post_type'         => 'itineraries',
-                'post__in'          => ( array ) $trip_ids,
-                'posts_per_page'    => 3,
-                'orderby'           => 'post__in',
-                'ignore_sticky_posts'   => true,
-                );                    
-        break;
-
-        default:
-        break;
-    }
-
-
-        // Run The Loop.
-        $query = new WP_Query( $args );
-        if ( $query->have_posts() ) : 
-            while ( $query->have_posts() ) : $query->the_post();
-                $page_post['id']     = get_the_ID();
-                $page_post['title']     = get_the_title();
-                $page_post['url']       = get_the_permalink();
-                $page_post['img']       = get_the_post_thumbnail_url( get_the_ID(),  'large' );
-                $page_post['excerpt']   = travel_ultimate_trim_content( 15 );
-
-                // Push to the main array.
-                array_push( $content, $page_post );
-            endwhile;
-        endif;
-        wp_reset_postdata();
-
+            // Push to the main array.
+            array_push( $content, $page_post );
+        endwhile;
+    endif;
+    wp_reset_postdata();
         
     if ( ! empty( $content ) ) {
         $input = $content;
     }
     return $input;
 }
-// package section content details.
-add_filter( 'travel_ultimate_filter_package_section_details', 'travel_ultimate_get_package_section_details' );
 
+// cta section content details.
+add_filter( 'travel_ultimate_filter_cta_section_details', 'travel_ultimate_get_cta_section_details' );
 
+function travel_ultimate_render_cta_section( $content_details = array() ) {
+    $options = travel_ultimate_get_theme_options();
+    $btn_label = ! empty( $options['cta_btn_label'] ) ? $options['cta_btn_label'] : esc_html__( 'Learn More', 'travel-ultimate' );
 
-?>
+    if ( empty( $content_details ) ) {
+        return;
+    } ?>
+
+    <div id="call-to-action" class="relative page-section">
+            <div class="wrapper">
+                <?php foreach ( $content_details as $content ) : ?>
+                    <article class="<?php echo ! empty( $content['image'] ) ? 'has' : 'no'; ?>-post-thumbnail">
+                        <div class="featured-image" style="background-image: url('<?php echo esc_url( $content['image'] ); ?>');">
+                        </div>
+
+                        <div class="cta-custom-wrapper">
+
+                           <!-- commenting out the title
+                                 <header class="entry-header">
+                                <h2 class="entry-title"><?php // echo esc_html( $content['title'] ); ?></h2>
+                            </header> -->
+
+                            <div class="cta-text-left">
+                                <p><?php echo wp_kses_post( $content['excerpt'] ); ?></p>
+                            </div><!-- .entry-content -->                                
+
+                            <?php if ( ! empty( $options['cta_btn_label'] ) ) : ?>
+                                <div class="cta-text-right">
+                                    <a href="<?php echo esc_url( $content['url'] ); ?>" class="btn"><?php echo esc_html( $options['cta_btn_label'] ); ?></a>
+                                </div><!-- .view-all -->
+                            <?php endif; ?>
+                        </div><!-- .entry-container -->
+                    </article>
+                    
+                <?php endforeach; ?>
+        </div><!-- .wrapper -->
+    </div><!-- .hero-cta-wrapper -->
+<?php }
