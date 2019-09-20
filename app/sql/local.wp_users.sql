4db08d9c2d4e2f5fbb048d3b6a1776b3128907fa
Calvinwuyts <jolanwuyts@gmail.com>
2019-09-20 15:27:23 +0200

    added to front page

--- app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"admin","$P$BGC4alQZ1WZofCvxCug69QbCW8gjkL/","admin","jolanwuyts@gmail.com","","2019-08-27 13:57:07","",0,"admin");
